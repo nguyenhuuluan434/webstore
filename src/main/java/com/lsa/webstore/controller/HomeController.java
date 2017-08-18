daef7f5953dddb1b301e5253fcb81deef18b42e6
Nguyen Huu Luan <nguyenhuuluan434@gmail.com>
2017-08-18 16:28:46 +0700

    update

---
+++ src/main/java/com/lsa/webstore/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.lsa.webstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Welcome to Web Store!");
+		model.addAttribute("tagline", "The one and only amazing web store");
+
+		return "welcome";
+	}
+
+	@RequestMapping("/")
+	public ModelAndView showMessage() {
+		System.out.println("in controller");
+		ModelAndView mv = new ModelAndView("welcome");
+		mv.addObject("greeting", "Welcome to Web Store!");
+		mv.addObject("tagline", "The one and only amazing web store");
+		return mv;
+	}
+}
